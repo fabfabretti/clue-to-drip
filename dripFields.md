ae5abdc220303bea153d8a64f55f6811855ea665
fabfabretti <59342704+fabfabretti@users.noreply.github.com>
2024-01-17 18:39:16 +0100

    import of bleeding data only works (and tested on drip. app).

---
+++ dripFields.md
@@ -0,0 +1,61 @@
+```js
+{
+  "temperature.value": "", // : float with 4.2f precision
+  "temperature.exclude": "", // : boolean
+  "temperature.time": "",// : time value with hh:mm format
+  "temperature.note": "",//freeform text
+  "bleeding.value":"",//int in [0,3]
+  "bleeding.exclude":"",//boolean
+  "mucus.feeling": "",//int in [0,3]
+  "mucus.texture": "",//int in [0,2]
+  "mucus.value": "",//int in [0,4]
+
+  /*· 0 = (dry feeling + no texture),
+    · 1 = (no feeling + no texture),
+    · 2 = (wet feeling + no texture),
+    · 3 = (no OR wet feeling + creamy texture),
+    · 4 = (any feeling + egg white texture) OR (slippery feeling + any texture).
+    null if either mucus.feeling OR mucus.texture are not present.
+  */
+ 
+  "mucus.exclude": "",//boolean
+  "cervix.opening": "",//int in [0,2]
+  "cervix.firmness": "",//int in [0,1]
+  "cervix.position": "",//int in [0,2]
+  "cervix.exclude": "",//boolean
+  "note.value": "", // freeform text
+  "desire.value": "",// int in [0,2]
+  "sex.solo": "",//boolean
+  "sex.partner": "",//boolean
+  "sex.condom": "",//boolean
+  "sex.pill": "",//boolean
+  "sex.iud": "",//boolean
+  "sex.patch": "",//boolean
+  "sex.ring": "",//boolean
+  "sex.implant": "",//boolean
+  "sex.diaphragm": "",//boolean
+  "sex.none": "",//boolean
+  "sex.other": "",//boolean
+  "sex.note": "",//freeform text
+  "pain.cramps": "",//boolean
+  "pain.ovulationPain": "",//boolean
+  "pain.headache": "",//boolean
+  "pain.backache": "",//boolean
+  "pain.nausea": "",//boolean
+  "pain.tenderBreasts": "",//boolean
+  "pain.migraine": "",//boolean
+  "pain.other": "",//boolean
+  "pain.note": "",//freeform text
+  "mood.happy": "",//boolean
+  "mood.sad": "",//boolean
+  "mood.stressed": "",//boolean
+  "mood.balanced": "",//boolean
+  "mood.fine": "",//boolean
+  "mood.anxious": "",//boolean
+  "mood.energetic": "",//boolean
+  "mood.fatigue": "",//boolean
+  "mood.angry": "",//boolean
+  "mood.other": "",//boolean
+  "mood.note": "",//freeform text
+}
+```
--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-import testData from './data/data.js'
-
-
 function convertFile() {
     const fileInput = document.getElementById('fileInput');
     const outputElement = document.getElementById('output');
@@ -12,7 +9,7 @@ function convertFile() {
 
         reader.onload = function (e) {
             // Perform your file conversion logic here
-            const convertedData = convertFunction(e.target.result);
+            const convertedData = clueToObjectList(e.target.result);
 
             // Display the converted data
             outputElement.innerText = convertedData;
@@ -31,39 +28,252 @@ function convertFile() {
     }
 }
 
-function convertFunction(dataString) {
-    const headerRow = "date,temperature.value,temperature.exclude,temperature.time,temperature.note,bleeding.value,bleeding.exclude,mucus.feeling,mucus.texture,mucus.value,mucus.exclude,cervix.opening,cervix.firmness,cervix.position,cervix.exclude,note.value,desire.value,sex.solo,sex.partner,sex.condom,sex.pill,sex.iud,sex.patch,sex.ring,sex.implant,sex.diaphragm,sex.none,sex.other,sex.note,pain.cramps,pain.ovulationPain,pain.headache,pain.backache,pain.nausea,pain.tenderBreasts,pain.migraine,pain.other,pain.note,mood.happy,mood.sad,mood.stressed,mood.balanced,mood.fine,mood.anxious,mood.energetic,mood.fatigue,mood.angry,mood.other,mood.note"
+function clueToObjectList(dataString) {
+    // drip. header columns and array of entries
+    let dripEntries = []
+
+    // parse Clue JSON
     var data = JSON.parse(dataString)
 
+    // Get all unique dates, bc in drip. there's one entry per date so we'll need that later
+    var entrydates = Array.from(new Set(data.map(entry => entry.date)))
 
-    console.log(data.filter(entry => entry.date === "2024-01-13"))
-    console.log(data)
+    //Equivalence between Clue and drip. bleeding value
+    var bleedingValues={"light":1,"medium":2,"heavy":3}
+
+    //Now for each unique clue date we want to generate one drip entry.
+    entrydates.forEach(date => 
+        {
+            // get the entries for current day only (is this wasteful af? yes. Do i care? no)
+            var curr_entries = data.filter(entry => entry.date === date)
+
+
+            // BELOW IS THE CORE OF THE PROGRAM. THIS IS THE CONVERSION
+            // FROM CLUE TO OBJECT IN MEMORY.
+
+            var dripEntry = {}
+
+
+            curr_entries.forEach(entry =>
+                    {
+                        switch(entry.type){
+                            case "period":
+                                dripEntry.bleedingValue = bleedingValues[entry.value.option]
+                                dripEntry.bleedingExclude = false 
+                                break;                            
+                        }
+                    }
+                
+                )
+
+            // We only save the generated entry if it has at least 1 field supported by drip.
+            if(!(Object.keys(dripEntry).length === 0)){
+                dripEntry.date = date
+                dripEntries.push(dripEntry)
+            }
+        });
+
+        return dripEntries
 }
 
 
-async function fetchTestData(convertFunction){
+function getAllValues(data){
+    console.log(Array.from(new Set(data.map(entry=>entry.type))))//type
+    console.log(Array.from(new Set(data.filter(entry=>entry.type === "pain")
+        .map(entry=>entry.value[0].option)))) //pain options
+    console.log(Array.from(new Set(data.filter(entry=>entry.type === "feelings")
+        .map(entry=>entry.value[0].option)))) //mood options
+    console.log(Array.from(new Set(data.filter(entry=>entry.type === "energy")
+        .map(entry=>entry.value[0].option)))) //energy options
+    console.log(Array.from(new Set(data.filter(entry=>entry.type === "tags")
+        .map(entry=>entry.value[0].option)))) //tags options
+}
+
+
+function writeToCSV(data){
+
+    var csvContent = "date,temperature.value,temperature.exclude,temperature.time,temperature.note,bleeding.value,bleeding.exclude,mucus.feeling,mucus.texture,mucus.value,mucus.exclude,cervix.opening,cervix.firmness,cervix.position,cervix.exclude,note.value,desire.value,sex.solo,sex.partner,sex.condom,sex.pill,sex.iud,sex.patch,sex.ring,sex.implant,sex.diaphragm,sex.none,sex.other,sex.note,pain.cramps,pain.ovulationPain,pain.headache,pain.backache,pain.nausea,pain.tenderBreasts,pain.migraine,pain.other,pain.note,mood.happy,mood.sad,mood.stressed,mood.balanced,mood.fine,mood.anxious,mood.energetic,mood.fatigue,mood.angry,mood.other,mood.note"
+
+
+    //insert a test with all the entry to check they're all correct
+    const data_complex = 
+        {
+            date:"2024-01-17",
+            temperatureValue: 35.75, // : float with 4.2f precision
+            temperatureExclude: false, // : boolean
+            temperatureTime: "10:10",// : time value with hh:mm format
+            temperatureNote: "note",//freeform text
+            bleedingValue:0,//int in [0,3]
+            bleedingExclude:false,//boolean
+            mucusFeeling: 0,//int in [0,3]
+            mucusTexture: 0,//int in [0,2]
+            mucusValue: 0,//int in [0,4]
+            mucusExclude: false,//boolean
+            cervixOpening: 0,//int in [0,2]
+            cervixFirmness: 0,//int in [0,1]
+            cervixPosition: 0,//int in [0,2]
+            cervixExclude: false,//boolean
+            noteValue: "note value", // freeform text
+            desireValue: 0,// int in [0,2]
+            sexSolo: false,//boolean
+            sexPartner:true,//boolean
+            sexCondom: false,//boolean
+            sexPill: false,//boolean
+            sexIud: false,//boolean
+            sexPatch: false,//boolean
+            sexRing: false,//boolean
+            sexImplant: false,//boolean
+            sexDiaphragm: false,//boolean
+            sexNone: false,//boolean
+            sexOther: false,//boolean
+            sexNote: "sex note",//freeform text
+            painCramps: false,//boolean
+            painOvulationPain: false,//boolean
+            painHeadache: false,//boolean
+            painBackache: false,//boolean
+            painNausea: false,//boolean
+            painTenderBreasts: false,//boolean
+            painMigraine: false,//boolean
+            painOther: false,//boolean
+            painNote: "pain note",//freeform text
+            moodHappy: false,//boolean
+            moodSad: false,//boolean
+            moodStressed: false,//boolean
+            moodBalanced: false,//boolean
+            moodFine: false,//boolean
+            moodAnxious: false,//boolean
+            moodEnergetic: false,//boolean
+            moodFatigue: false,//boolean
+            moodAngry: false,//boolean
+            moodOther: false,//boolean
+            moodNote: "mood note",//freeform text
+          }
+
+
+
+    const data_simple = {
+        date:"2024-01-17",
+        bleedingValue : 2,
+        bleedingExclude : false}
+
+
+
+    data.forEach(entry => {
+        csvContent+=("\n")
+        csvContent+=(entry.date)+","
+
+
+        entry.temperatureValue !== undefined ? csvContent+=entry.temperatureValue+"," : csvContent+=","
+        entry.temperatureExclude !== undefined ? csvContent+=entry.temperatureExclude+"," : csvContent+=","
+
+        entry.temperatureTime !== undefined ? csvContent+=entry.temperatureTime+"," : csvContent+=","
+        entry.temperatureNote !== undefined ? csvContent+=entry.temperatureNote+"," : csvContent+=","
+
+
+        entry.bleedingValue !== undefined ? csvContent+=entry.bleedingValue+"," : csvContent+=","
+        entry.bleedingExclude !== undefined ? csvContent+=entry.bleedingExclude+"," : csvContent+=","
+
+
+        entry.mucusFeeling !== undefined ? csvContent+=entry.mucusFeeling+"," : csvContent+=","
+        entry.mucusTexture !== undefined ? csvContent+=entry.mucusTexture+"," : csvContent+=","
+        entry.mucusValue !== undefined ? csvContent+=entry.mucusValue+"," : csvContent+=","
+        entry.mucusExclude !== undefined ? csvContent+=entry.mucusExclude+"," : csvContent+=","
+
+
+        entry.cervixOpening !== undefined ? csvContent+=entry.cervixOpening+"," : csvContent+=","
+        entry.cervixFirmness !== undefined ? csvContent+=entry.cervixFirmness+"," : csvContent+=","
+        entry.cervixPosition !== undefined ? csvContent+=entry.cervixPosition+"," : csvContent+=","
+        entry.cervixExclude !== undefined ? csvContent+=entry.cervixExclude+"," : csvContent+=","
+
+        entry.noteValue !== undefined ? csvContent+=entry.noteValue+"," : csvContent+=","
+        
+        entry.desireValue !== undefined ? csvContent+=entry.desireValue+"," : csvContent+=","
+
+        entry.sexSolo !== undefined ? csvContent+=entry.sexSolo+"," : csvContent+=","
+        entry.sexPartner !== undefined ? csvContent+=entry.sexPartner+"," : csvContent+=","
+        entry.sexCondom !== undefined ? csvContent+=entry.sexCondom+"," : csvContent+=","
+        entry.sexPill !== undefined ? csvContent+=entry.sexPill+"," : csvContent+=","
+        entry.sexIud !== undefined ? csvContent+=entry.sexIud+"," : csvContent+=","
+        entry.sexPatch !== undefined ? csvContent+=entry.sexPatch+"," : csvContent+=","
+        entry.sexRing !== undefined ? csvContent+=entry.sexRing+"," : csvContent+=","
+        entry.sexImplant !== undefined ? csvContent+=entry.sexImplant+"," : csvContent+=","
+        entry.sexDiaphgram !== undefined ? csvContent+=entry.sexDiaphgram+"," : csvContent+=","
+        entry.sexNone !== undefined ? csvContent+=entry.sexNone+"," : csvContent+=","
+        entry.sexOther !== undefined ? csvContent+=entry.sexOther+"," : csvContent+=","
+        entry.sexNote !== undefined ? csvContent+=entry.sexNote+"," : csvContent+=","
+
+        entry.painCramps !== undefined ? csvContent+=entry.painCramps+"," : csvContent+=","
+        entry.painOvulationPain !== undefined ? csvContent+=entry.painOvulationPain+"," : csvContent+=","
+        entry.painHeadache !== undefined ? csvContent+=entry.painHeadache+"," : csvContent+=","
+        entry.painBackache !== undefined ? csvContent+=entry.painBackache+"," : csvContent+=","
+        entry.painNausea !== undefined ? csvContent+=entry.painNausea+"," : csvContent+=","
+        entry.painTenderBreasts !== undefined ? csvContent+=entry.painTenderBreasts+"," : csvContent+=","
+        entry.painMigraine !== undefined ? csvContent+=entry.painMigraine+"," : csvContent+=","
+        entry.painOther !== undefined ? csvContent+=entry.painOther+"," : csvContent+=","
+        entry.painNote !== undefined ? csvContent+=entry.painNote+"," : csvContent+=","
+
+        entry.moodHappy !== undefined ? csvContent+=entry.moodHappy+"," : csvContent+=","
+        entry.moodSad !== undefined ? csvContent+=entry.moodSad+"," : csvContent+=","
+        entry.moodStressed !== undefined ? csvContent+=entry.moodStressed+"," : csvContent+=","
+        entry.moodBalanced !== undefined ? csvContent+=entry.moodBalanced+"," : csvContent+=","
+        entry.moodFine !== undefined ? csvContent+=entry.moodFine+"," : csvContent+=","
+        entry.moodAnxious !== undefined ? csvContent+=entry.moodAnxious+"," : csvContent+=","
+        entry.moodEnergetic !== undefined ? csvContent+=entry.moodEnergetic+"," : csvContent+=","
+        entry.moodFatigue !== undefined ? csvContent+=entry.moodFatigue+"," : csvContent+=","
+        entry.moodAngry !== undefined ? csvContent+=entry.moodAngry+"," : csvContent+=","
+        entry.moodOther !== undefined ? csvContent+=entry.moodOther+"," : csvContent+=","
+        entry.moodNote !== undefined ? csvContent+=entry.moodNote+"," : csvContent+=","
+
+        // remove last comma
+        csvContent = csvContent.slice(0,-1)
+    
+    })
+
+    console.log(csvContent)
+ 
+
+
+    const dataUri = "data:text/csv;charset=utf8,"+ encodeURIComponent(csvContent)
+
+    const link = document.createElement("a")
+    link.setAttribute("href",dataUri)
+    link.setAttribute("download","tf.csv")
+    document.body.appendChild(link)
+
+    document.getElementById('downloadButton').addEventListener('click', () => {
+        link.click();
+    });
+
+    document.getElementById("output").innerHTML=csvContent
+
+}
+
+async function fetchTestData(convertFunction,writeFunction){
+    //location of test file
     var filePath = "./data/measurements.json"
+
     var data = null
+
     try{
         const response = await fetch(filePath)
-        console.log(response.ok)
+        
         if(!response.ok){
-            throw new Error('Porcodio');
+            throw new Error('There were issues loading the test file...');
         }
         else{
-            data = await response.text();
+            data = await response.text()
             data = convertFunction(data)
+            console.log("Clue file loaded correctly.")
+            data = writeFunction(data)
         }
 
     }catch(error){
-        console.error("Porcodio rilevato")
+        console.error("There were issues.")
         throw error;
     }
 }
 
 
 
-
-fetchTestData(convertFunction)
-
+ var data = await fetchTestData(clueToObjectList,writeToCSV)
 
