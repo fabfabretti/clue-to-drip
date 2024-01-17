651d52d6989738ffcd763ed93f2145b2dd663fe0
fabfabretti <fabretti.fabiola@gmail.com>
2024-01-17 01:52:54 +0100

    painfully, now it loads the json correctly

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import testData from './data/data.js'
+
+
 function convertFile() {
     const fileInput = document.getElementById('fileInput');
     const outputElement = document.getElementById('output');
@@ -19,12 +22,48 @@ function convertFile() {
         reader.readAsText(file);
     } else {
         outputElement.innerText = 'Please choose a file';
+
+
+
+
+
+
     }
 }
 
-function convertFunction(data) {
-    // Implement your file conversion logic here
-    // For example, you can convert CSV to JSON, etc.
-    // This is just a placeholder function
-    return 'Converted data: ' + data;
+function convertFunction(dataString) {
+    const headerRow = "date,temperature.value,temperature.exclude,temperature.time,temperature.note,bleeding.value,bleeding.exclude,mucus.feeling,mucus.texture,mucus.value,mucus.exclude,cervix.opening,cervix.firmness,cervix.position,cervix.exclude,note.value,desire.value,sex.solo,sex.partner,sex.condom,sex.pill,sex.iud,sex.patch,sex.ring,sex.implant,sex.diaphragm,sex.none,sex.other,sex.note,pain.cramps,pain.ovulationPain,pain.headache,pain.backache,pain.nausea,pain.tenderBreasts,pain.migraine,pain.other,pain.note,mood.happy,mood.sad,mood.stressed,mood.balanced,mood.fine,mood.anxious,mood.energetic,mood.fatigue,mood.angry,mood.other,mood.note"
+    var data = JSON.parse(dataString)
+
+
+    console.log(data.filter(entry => entry.date === "2024-01-13"))
+    console.log(data)
 }
+
+
+async function fetchTestData(convertFunction){
+    var filePath = "./data/measurements.json"
+    var data = null
+    try{
+        const response = await fetch(filePath)
+        console.log(response.ok)
+        if(!response.ok){
+            throw new Error('Porcodio');
+        }
+        else{
+            data = await response.text();
+            data = convertFunction(data)
+        }
+
+    }catch(error){
+        console.error("Porcodio rilevato")
+        throw error;
+    }
+}
+
+
+
+
+fetchTestData(convertFunction)
+
+
